8324a112153d7ec5d3ae60555e6992500d177666
jessicahylee <jehyerinn@gmail.com>
2023-10-10 12:04:51 -0400

    Delete component

---
+++ src/components/warehouse/delete/delete.jsx
@@ -0,0 +1,43 @@
+import './delete.scss'
+
+function Delete() {
+  return (
+    <div className="delete__container">
+      <button className="delete__button-x"> X </button>
+      <div className="container__delete">
+        <div className="delete__container-title">
+          <h2 className="delete__container-title-paragraph">
+            Delete Washington warehouse?
+          </h2>
+        </div>
+        <div className="delete__container-paragraph">
+          <p className="delete__container-p">
+            Please confirm that you'd like to delete the Washignton from the
+            list of warehouses. You won't be able to undo this action.
+          </p>
+        </div>
+        <div className="container__delete--buttons">
+          <div className="delete__container-btn">
+            <button className="buttonCancel">Cancel</button>
+          </div>
+          <div className="delete__container-btn1">
+            <button className="buttonDelete">Delete</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Delete
+
+//function Header() {
+// Pop an alert mentioning: Are you sure you wanna delete it.
+// On Click Handler near the button (icon) delete and run a function Delete item //<button onClick={() => deleteItems(items.id)}>Delete</button> // You write this part on RETURN
+// Create a function deleteItems(id) =>{
+// const updateItems = [...Items].filter((items)=>items.id !== id)
+// setItems(updateItems)
+// }
+//}
+// Delete Washington warehouse? Please confirm that you'd like to delete the Washington from the list of warehouse. You won't be able to undo this action
+// Button CANCEL
+// Button DELETE
---
+++ src/components/warehouse/edit/edit.jsx
@@ -0,0 +1,5 @@
+// Create a hook to Edit: 
+// const [Editing, setEditing] = React.useState(null)
+// const [EditingText, setEditingText] = React.useState('') 
+// On Button add onClick =>{()=> setEditingText(todo.id)}
+// Add a FORM
